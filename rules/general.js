4f51750fdfd9f4841616178789282279cac7c745
Craig Baldwin <craig@wemakewebsites.com>
2020-04-25 11:26:04 +0100

    BUILD: Removed linebreaks rule so code can be OS agnostic

--- rules/general.js
+++ rules/general.js
@@ -2,7 +2,7 @@ module.exports = {
   // Specify indentation.
   indentation: 2,
   // Specify unix or windows linebreaks
-  linebreaks: 'unix',
+  linebreaks: null,
   // Disallow more than a specified number of adjacent empty lines.
   'max-empty-lines': 1,
   // Limit the length of a line.
